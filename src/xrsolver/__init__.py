ae96939267615faf0457e7a766f938beac96f577
Xanadu Wang <xanaduw@synology.com>
2022-01-01 19:58:25 +0800

    提供適當的 API 以供外界使用。

--- src/xrsolver/__init__.py
+++ src/xrsolver/__init__.py
@@ -1,4 +1,7 @@
+from .core.constraint import CompoundConstraint
 from .core.problem import Problem
+from .core.problem import Objective
+from .core.constants import Optimization
 from .core.solver import AbsSolver
 
 __import__('pkg_resources').declare_namespace(__name__)
